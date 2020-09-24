6cd1dc23551b4b888160dd389d5e6e99323b1dfc
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-09-24 20:44:26 +0200

    Dont upsample auxilary estimate

--- torchvision/models/segmentation/_utils.py
+++ torchvision/models/segmentation/_utils.py
@@ -28,7 +28,8 @@ class _SimpleSegmentationModel(nn.Module):
         if self.aux_classifier is not None:
             x = features["aux"]
             x = self.aux_classifier(x)
-            x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+            # Keep auxiliary output at predicted size
+            # x = F.interpolate(x, size=input_shape, mode='nearest', align_corners=False)
             result["aux"] = x
 
         return result
